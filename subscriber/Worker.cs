7353943fdb61ca4743dcb474add7d02e2b58c7e5
Tate Button <buttontate@gmail.com>
2020-04-22 15:09:34 -0500

    subscriber

--- subscriber/Worker.cs
+++ subscriber/Worker.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using Google.Cloud.PubSub.V1;
+using Grpc.Core;
+using Microsoft.Extensions.FileSystemGlobbing.Abstractions;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
@@ -11,6 +14,9 @@ namespace subscriber
     public class Worker : BackgroundService
     {
         private readonly ILogger<Worker> _logger;
+        private const string GCP_PROJECT = "emulator";
+        private const string GCP_SUBSCRIBER = "item-change-subscriber";
+        private const string GCP_TOPIC = "item-change";
 
         public Worker(ILogger<Worker> logger)
         {
@@ -22,8 +28,59 @@ namespace subscriber
             while (!stoppingToken.IsCancellationRequested)
             {
                 _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                if (CanConnectToPubSub())
+                {
+                    // Subscribe to the topic.
+                    SubscriberServiceApiClient subscriber = CreateSubscriber();
+                    var subscriptionName = new SubscriptionName(GCP_PROJECT,GCP_SUBSCRIBER);
+                
+                    PullResponse response = subscriber.Pull(subscriptionName, returnImmediately: true, maxMessages: 100);
+                    foreach (ReceivedMessage received in response.ReceivedMessages)
+                    {
+                        PubsubMessage msg = received.Message;
+                        Console.WriteLine($"Received message {msg.MessageId} published at {msg.PublishTime.ToDateTime()}");
+                        Console.WriteLine($"Text: '{msg.Data.ToStringUtf8()}'");
+                        
+                    }
+                    subscriber.Acknowledge(subscriptionName, response.ReceivedMessages.Select(m => m.AckId));
+                }
+                
                 await Task.Delay(5000, stoppingToken);
             }
+        }
+        
+        private SubscriberServiceApiClient CreateSubscriber()
+        {
+            var subscriberBuilder = new SubscriberServiceApiClientBuilder();
+            subscriberBuilder.ChannelCredentials = ChannelCredentials.Insecure;
+            subscriberBuilder.Endpoint = Environment.GetEnvironmentVariable("PUBSUB_EMULATOR_HOST");
+            var subscriber = subscriberBuilder.Build();
+            return subscriber;
+        }
+        
+        private bool CanConnectToPubSub()
+        {
+            
+
+            try
+            {
+                var subscriber = CreateSubscriber();
+                SubscriptionName subscriptionName = new SubscriptionName(GCP_PROJECT, GCP_SUBSCRIBER);
+                subscriber.CreateSubscription(subscriptionName,new TopicName(GCP_PROJECT, GCP_TOPIC), pushConfig: null, ackDeadlineSeconds: 60);
+            }
+            catch (RpcException e)
+                when (e.Status.StatusCode == StatusCode.Unavailable)
+            {
+                _logger.LogWarning($"Unable to connect to pub sub");
+                return false;
+            }
+            catch (RpcException e)
+                when (e.Status.StatusCode == StatusCode.AlreadyExists)
+            {
+                return true;
+            }
+            
+            return true;
         }
     }
 }
